dc67c873d4cf344b557ec4090a3e93c3324d8dbc
Danila <danila10danila@mail.ru>
2023-04-23 13:44:03 +0300

    feat: tests for receiveAttack()

--- __tests__/gameboardFactory.test.js
+++ __tests__/gameboardFactory.test.js
@@ -21,8 +21,8 @@ test('Gameboard factory: places ship horizontally', () => {
 
     gameboard.placeShip(x, y, shipLength, orientation);
 
-    expect(gameboard.cells[x][y]).not.toBe(0 || 1);
-    expect(gameboard.cells[x + 1][y]).not.toBe(0 || 1);
+    expect(gameboard.cells[x][y]).not.toBe(0);
+    expect(gameboard.cells[x + 1][y]).not.toBe(0);
 })
 
 test('Gameboard factory: places ship vertically', () => {
@@ -36,8 +36,8 @@ test('Gameboard factory: places ship vertically', () => {
 
     gameboard.placeShip(x, y, shipLength, orientation);
     
-    expect(gameboard.cells[x][y]).not.toBe(0 || 1);
-    expect(gameboard.cells[x][y + 1]).not.toBe(0 || 1);
+    expect(gameboard.cells[x][y]).not.toBe(0);
+    expect(gameboard.cells[x][y + 1]).not.toBe(0);
 })
 
 test('Gameboard factory: ship does not go outbounds horizontally', () => {
@@ -62,4 +62,56 @@ test('Gameboard factory: ship does not go outbounds vertically', () => {
     let orientation = "vertical";
 
     expect(() => { gameboard.placeShip(x, y, shipLength, orientation); }).toThrow("Out of bounds: vertical");
+})
+
+test('Gameboard factory: ship receive attacks correctly, happy path', () => {
+    let length = 5;
+    let gameboard = gameboardFactory(length);
+    
+    let x = 2;
+    let y = 2;
+    let shipLength = 2;
+    let orientation = "vertical";
+
+    gameboard.placeShip(x, y, shipLength, orientation)
+    gameboard.receiveAttack(x, y)
+    gameboard.receiveAttack(x, y + 1)
+    expect(gameboard.cells[x][y].isSunk()).toBe(true);
+})
+
+test('Gameboard factory: cant hit same spot with ship twice', () => {
+    let length = 5;
+    let gameboard = gameboardFactory(length);
+    
+    let x = 2;
+    let y = 2;
+    let shipLength = 2;
+    let orientation = "vertical";
+
+    gameboard.placeShip(x, y, shipLength, orientation)
+    gameboard.receiveAttack(x, y)
+    expect(() => { gameboard.receiveAttack(x, y); }).toThrow("Cant hit same cell twice");
+})
+
+test('Gameboard factory: cant hit same spot without ship twice', () => {
+    let length = 5;
+    let gameboard = gameboardFactory(length);
+    
+    let x = 4;
+    let y = 4;
+
+    gameboard.receiveAttack(x, y)
+    expect(() => { gameboard.receiveAttack(x, y); }).toThrow("Cant hit same cell twice");
+})
+
+test('Gameboard factory: can hit any cell', () => {
+    let length = 5;
+    let gameboard = gameboardFactory(length);
+
+    expect(() => { gameboard.receiveAttack(0, 0); }).not.toThrow("Cant hit same cell twice");
+    expect(() => { gameboard.receiveAttack(0, 1); }).not.toThrow("Cant hit same cell twice");
+    expect(() => { gameboard.receiveAttack(1, 0); }).not.toThrow("Cant hit same cell twice");
+    expect(() => { gameboard.receiveAttack(1, 1); }).not.toThrow("Cant hit same cell twice");
+    expect(() => { gameboard.receiveAttack(0, 2); }).not.toThrow("Cant hit same cell twice");
+    expect(() => { gameboard.receiveAttack(2, 1); }).not.toThrow("Cant hit same cell twice");
 })

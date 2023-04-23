f2f2981879e1603896774b05f61d7c7ff3e489eb
Danila <danila10danila@mail.ru>
2023-04-23 13:01:47 +0300

    feat: shipFactory covered in tests

--- __tests__/shipFactory.test.js
+++ __tests__/shipFactory.test.js
@@ -1,10 +1,30 @@
 import { shipFactory } from "../src/shipFactory";
 import { test } from '@jest/globals';
 
-test('Ship factory: sunking works', () => {
+test('Ship factory: sunking works (1)', () => {
     let ship = shipFactory(3);
     ship.hit()
     ship.hit()
     ship.hit()
-    expect(ship.isSunk).toBe(true);
+    expect(ship.isSunk()).toBe(true);
+})
+
+test('Ship factory: sunking works (2)', () => {
+    let ship = shipFactory(2);
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBe(true);
+})
+
+test('Ship factory: ship is alive (1)', () => {
+    let ship = shipFactory(3);
+    ship.hit()
+    expect(ship.isSunk()).toBe(false);
+})
+
+test('Ship factory: ship is alive (2)', () => {
+    let ship = shipFactory(3);
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBe(false);
 })

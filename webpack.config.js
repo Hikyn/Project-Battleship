55ade280c44daa528725faf76382dd95eb1954db
Danila <danila10danila@mail.ru>
2023-04-25 14:57:58 +0300

    feat: favicon and title

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,12 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader'
+                }
             }
         ]
     }

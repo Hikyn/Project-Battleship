450b49d25b02dbb02597a5a8e36841003a94f253
Danila <danila10danila@mail.ru>
2023-04-27 14:08:02 +0300

    test: player isAllShipsPlaced

--- __tests__/playerFactory.test.js
+++ __tests__/playerFactory.test.js
@@ -1,4 +1,17 @@
-import { test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import { playerFactory } from '../src/playerFactory';
+import { gameboardFactory } from '../src/gameboardFactory';
 
-test.skip('empty', () => {});
+test('Player: check if all ships are placed', () => {
+    const gameboard1 = gameboardFactory(6, [5, 4, 3, 2]);
+    let target = '.gameboard-self';
+    const player1 = playerFactory('Me', gameboard1, target);
+    expect(player1.isAllShipsPlaced()).toBe(false);
+
+    player1.gameboard.placeShip(0, 0, 5, 'horizontal');
+    player1.gameboard.placeShip(0, 1, 4, 'horizontal');
+    player1.gameboard.placeShip(0, 2, 3, 'horizontal');
+    player1.gameboard.placeShip(0, 3, 2, 'horizontal');
+
+    expect(player1.isAllShipsPlaced()).toBe(true);
+});

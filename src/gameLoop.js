422db655efcbe8a308ec6fbe742d3b292ba55afe
Danila <danila10danila@mail.ru>
2023-04-27 16:08:03 +0300

    changed logic to ease pop-up creation

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -7,18 +7,20 @@ function gameloop(gameboardLength) {
     // Dummy data
     const gameboard1 = gameboardFactory(gameboardLength);
     let target = document.querySelector('.gameboard-self');
-    const player1 = playerFactory('Me', gameboard1, target);
+    const player1 = playerFactory('Hikyn', gameboard1, target);
 
-    renderer.renderGameboard(player1);
-    renderer.renderAvailableShips(player1);
+    // renderer.renderGameboard(player1);
+    // renderer.renderAvailableShips(player1);
+
+    renderer.renderPlacementScreen(player1);
 
     const gameboard2 = gameboardFactory(gameboardLength);
     target = document.querySelector('.gameboard-enemy');
     const player2 = playerFactory('Computer', gameboard2, target, true);
     player2.gameboard.randomlyPlaceAllShips();
 
-    renderer.renderGameboard(player2);
-    renderer.listenForAttacks(player2, player1);
+    // renderer.renderGameboard(player2);
+    // renderer.listenForAttacks(player2, player1);
 }
 
 export { gameloop };
